6f83eab736819c577baf5e16d5a0117f2a6ef0c7
phbrugnolo <phbrugnolo@gmail.com>
2025-06-07 19:30:51 -0300

    Ajustando o autoload

--- autoload.php
+++ autoload.php
@@ -1,14 +1,47 @@
 <?php
 spl_autoload_register(function ($namespace) {
-    $arquivo = __DIR__ . DIRECTORY_SEPARATOR . str_replace(
+    $relativePath = str_replace(
         ['App\\', '\\'],
         ['', DIRECTORY_SEPARATOR],
         $namespace
     ) . '.php';
 
-    if (file_exists($arquivo)) {
-        require_once $arquivo;
+    $file = __DIR__ . DIRECTORY_SEPARATOR . $relativePath;
+
+    if (file_exists($file)) {
+        require_once $file;
+        return;
+    }
+
+    $file = __DIR__ . DIRECTORY_SEPARATOR . strtolower($relativePath);
+
+    if (file_exists($file)) {
+        require_once $file;
+        return;
+    }
+
+    $parts = explode(DIRECTORY_SEPARATOR, $relativePath);
+    $currentPath = __DIR__;
+
+    foreach ($parts as $part) {
+        if ($part === end($parts)) {
+            $files = glob($currentPath . DIRECTORY_SEPARATOR . '*');
+            foreach ($files as $potentialFile) {
+                if (strtolower(basename($potentialFile)) === strtolower($part)) {
+                    require_once $potentialFile;
+                    return;
+                }
+            }
+        } else {
+            $dirs = glob($currentPath . DIRECTORY_SEPARATOR . '*', GLOB_ONLYDIR);
+            foreach ($dirs as $dir) {
+                if (strtolower(basename($dir)) === strtolower($part)) {
+                    $currentPath = $dir;
+                    break;
+                }
+            }
+        }
     }
 });
 
-require_once __DIR__ . '/helpers/Functions.php';
+require_once __DIR__ . '/helpers/functions.php';
