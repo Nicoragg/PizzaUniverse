100ab7784690be27a3813cd5ae1fd14eddc419ed
phbrugnolo <phbrugnolo@gmail.com>
2025-06-07 20:12:57 -0300

    Melhorando header

--- views/components/header.php
+++ views/components/header.php
@@ -12,14 +12,9 @@
       <ul class="nav-list">
         <li><a href="?page=home" class="nav-link">Página Inicial</a></li>
         <li><a href="?page=menu" class="nav-link">Cardápio</a></li>
+        <li><a href="?page=users" class="nav-link">Usuários</a></li>
         <li><a href="?page=deliver" class="nav-link cta-button">Peça Agora</a></li>
       </ul>
     </nav>
-
-    <button class="mobile-menu-toggle" aria-label="Menu mobile">
-      <span class="hamburger-line"></span>
-      <span class="hamburger-line"></span>
-      <span class="hamburger-line"></span>
-    </button>
   </div>
 </header>
--- views/pages/default.php
+++
@@ -1,2 +0,0 @@
-<h1>404 Not Found</h1>
-<h2>Página não encontrada na Galáxia</h2>
